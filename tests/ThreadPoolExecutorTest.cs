0ddc02e864810b563c1194a8f2b4668baa01ec63
mapogolions <ivanovpavelalex45@gmail.com>
2023-09-09 02:49:18 +0500

    arrange act assert

--- tests/ThreadPoolExecutorTest.cs
+++ tests/ThreadPoolExecutorTest.cs
@@ -5,6 +5,7 @@ public class ThreadPoolExecutorTest
     [Fact]
     public void ShouldSupportMultipleShutdowns()
     {
+        // Arrange
         var executor = new ThreadPoolExecutor(4);
         var future = executor.Submit<string>(s => (string)s!, "foo");
         var t = new Thread(s =>
@@ -12,10 +13,13 @@ public class ThreadPoolExecutorTest
             Thread.Sleep(TimeSpan.FromMilliseconds(200));
             executor.Shutdown(wait: true);
         });
+
+        // Act
         t.Start();
         executor.Shutdown(wait: false);
         t.Join();
 
+        // Assert
         Assert.Equal("foo", future.GetResult());
         Assert.Equal(1, executor.SpawnedThreads);
     }
@@ -23,14 +27,17 @@ public class ThreadPoolExecutorTest
     [Fact]
     public void ShutdownShouldWakeUpThreads()
     {
+        // Arrange
         var executor = new ThreadPoolExecutor(4);
         var future = executor.Submit<string>(s => (string)s!, "foo");
         var result = future.GetResult();
 
+        // Act
         // Give a spawned thread some time to try to take the next element from the BlockingCollection
         Thread.Sleep(TimeSpan.FromMilliseconds(100));
         executor.Shutdown(wait: true);
 
+        // Assert
         Assert.Equal("foo", result);
         Assert.Equal(1, executor.SpawnedThreads);
     }
@@ -38,6 +45,7 @@ public class ThreadPoolExecutorTest
     [Fact]
     public void ShouldComplete_N_Futures()
     {
+        // Arrange
         var executor = new ThreadPoolExecutor();
         static string callback(object? s)
         {
@@ -47,8 +55,11 @@ public class ThreadPoolExecutorTest
         var futures = Enumerable.Range(1, 30)
             .Select(x => executor.Submit<string>(callback, $"task-{x}"))
             .ToList();
+
+        // Act
         var done = Future.Wait<string>(FutureWaitPolicy.AllCompleted, futures.ToArray());
 
+        // Assert
         Assert.Equal(30, done.Count);
     }
 }
